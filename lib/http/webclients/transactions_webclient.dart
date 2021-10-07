41aaf960c98a6e2683d426b78f046755dac7468d
guerraglucas <lucasguerra34@gmail.com>
2021-10-07 14:56:51 -0300

    refactor

--- lib/http/webclients/transactions_webclient.dart
+++ lib/http/webclients/transactions_webclient.dart
@@ -7,7 +7,9 @@ class TransactionWebclient {
   Future<List<Transaction>> findAll() async {
     final http.Response response =
         await client.get(baseUrl).timeout(Duration(seconds: 10));
-    List<Transaction> transactions = _toListOfMap(response);
+    List<Transaction> transactions = jsonDecode(response.body)
+        .map((dynamic json) => Transaction.fromJson(json))
+        .toList();
 
     return transactions;
   }
@@ -17,20 +19,6 @@ class TransactionWebclient {
     final http.Response response = await client.post(baseUrl,
         headers: {'Content-type': 'application/json', 'password': '1000'},
         body: transactionJson);
-    return _toTransaction(response);
-  }
-
-  List<Transaction> _toListOfMap(http.Response response) {
-    final List<dynamic> decodedJson = jsonDecode(response.body);
-    final List<Transaction> transactions = [];
-    for (Map<String, dynamic> transactionMap in decodedJson) {
-      transactions.add(Transaction.fromJson(transactionMap));
-    }
-    return transactions;
-  }
-
-  Transaction _toTransaction(http.Response response) {
-    final Map<String, dynamic> json = jsonDecode(response.body);
-    return Transaction.fromJson(json);
+    return Transaction.fromJson(jsonDecode(response.body));
   }
 }
